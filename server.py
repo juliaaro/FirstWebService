ea60f57ff3fcb90f958783f47abf15e51bdab0fc
Julia Aro <78003709+juliaaro@users.noreply.github.com>
2021-02-10 18:54:31 -0300

    Change server return to JSON and add front end to interact with it.

--- server.py
+++ server.py
@@ -6,6 +6,28 @@ from flask import request
 app = flask.Flask('jolha')
 app.config["DEBUG"] = True
 
+info = {
+        "salario_bruto": "",
+        "ir": "",
+        "inss": "",
+        "sindicato": "",
+        "salario_liquido": ""
+    }
+
+def preenche(bruto, ir, inss, sindicato, liquido):
+    info["salario_bruto"] = float(format(bruto, '.2f'))
+    info["ir"] = float(format(ir, '.2f'))
+    info["inss"] = float(format(inss, '.2f'))
+    info["sindicato"] = float(format(sindicato, '.2f'))
+    info["salario_liquido"] = float(format(liquido, '.2f'))
+
+def calcula(horas, valor):
+    bruto = valor * horas
+    ir = bruto * 0.11
+    inss = bruto * 0.08
+    sindicato = bruto * 0.05
+    liquido = bruto - ir - inss - sindicato
+    return bruto, ir, inss, sindicato, liquido
 
 @app.route('/', methods=['GET'])
 def home():
@@ -16,26 +38,11 @@ def home():
     else:
         return "Error: No \'valor\' or \'horas\' field provided. Please specify an them."
 
-    bruto = valor * horas
-    ir = bruto * 0.11
-    inss = bruto * 0.08
-    sindicato = bruto * 0.05
+    bruto, ir, inss, sindicato, liquido = calcula(horas, valor)
+    preenche(bruto, ir, inss, sindicato, liquido)
 
-    liquido = bruto - ir - inss - sindicato
-
-    info = {
-        "salario_bruto": float(format(bruto, '.2f')),
-        "ir": float(format(ir, '.2f')),
-        "inss": float(format(inss, '.2f')),
-        "sindicato": float(format(sindicato, '.2f')),
-        "salario_liquido": float(format(liquido, '.2f'))
-    }
-
-    json_file = json.dumps(info)
-    #return json_file
-    data = "<h1>Salário Bruto: R$ " + str(info['salario_bruto']) + "</h1>" + "<p>- IR (11%) : R$ " + str(info["ir"]) + "</p>" + "<p>- INSS (8%) : R$ " + str(info["inss"]) + "</p>" + "<p>- Sindicato (5%) : R$ " + str(info["sindicato"]) + "</p>" + "<p>- Salário Líquido : R$ " + str(info["salario_liquido"]) + "</p>"
-
-    return data 
+    #data = "<h1>Salário Bruto: R$ " + str(info['salario_bruto']) + "</h1>" + "<p>- IR (11%) : R$ " + str(info["ir"]) + "</p>" + "<p>- INSS (8%) : R$ " + str(info["inss"]) + "</p>" + "<p>- Sindicato (5%) : R$ " + str(info["sindicato"]) + "</p>" + "<p>- Salário Líquido : R$ " + str(info["salario_liquido"]) + "</p>"
+    return json.dumps(info)
 
 @app.route('/bruto', methods=['GET'])
 def bruto():
@@ -46,24 +53,11 @@ def bruto():
     else:
         return "Error: No id field provided. Please specify an id."
 
-    bruto = valor * horas
-    ir = bruto * 0.11
-    inss = bruto * 0.08
-    sindicato = bruto * 0.05
-
-    liquido = bruto - ir - inss - sindicato
-
-    info = {
-        "salario_bruto": float(format(bruto, '.2f')),
-        "ir": float(format(ir, '.2f')),
-        "inss": float(format(inss, '.2f')),
-        "sindicato": float(format(sindicato, '.2f')),
-        "salario_liquido": float(format(liquido, '.2f'))
-    }
-
-    data = "<h1>Salário Bruto: R$ " + str(info['salario_bruto']) + "</h1>"
-
-    return data
+    bruto, ir, inss, sindicato, liquido = calcula(horas, valor)
+    preenche(bruto, ir, inss, sindicato, liquido)
+    #data = "<h1>Salário Bruto: R$ " + str(info['salario_bruto']) + "</h1>"
+    json_file = json.dumps({"salario_bruto": info["salario_bruto"]})
+    return json_file
 
 @app.route('/ir', methods=['GET'])
 def ir():
@@ -74,24 +68,11 @@ def ir():
     else:
         return "Error: No id field provided. Please specify an id."
 
-    bruto = valor * horas
-    ir = bruto * 0.11
-    inss = bruto * 0.08
-    sindicato = bruto * 0.05
+    bruto, ir, inss, sindicato, liquido = calcula(horas, valor)
+    preenche(bruto, ir, inss, sindicato, liquido)
+    #data = "<h1>IR: R$ " + str(info['ir']) + "</h1>"
 
-    liquido = bruto - ir - inss - sindicato
-
-    info = {
-        "salario_bruto": float(format(bruto, '.2f')),
-        "ir": float(format(ir, '.2f')),
-        "inss": float(format(inss, '.2f')),
-        "sindicato": float(format(sindicato, '.2f')),
-        "salario_liquido": float(format(liquido, '.2f'))
-    }
-
-    data = "<h1>IR: R$ " + str(info['ir']) + "</h1>"
-
-    return data
+    return json.dumps({"ir": info["ir"]})
 
 @app.route('/inss', methods=['GET'])
 def inss():
@@ -102,24 +83,11 @@ def inss():
     else:
         return "Error: No id field provided. Please specify an id."
 
-    bruto = valor * horas
-    ir = bruto * 0.11
-    inss = bruto * 0.08
-    sindicato = bruto * 0.05
+    bruto, ir, inss, sindicato, liquido = calcula(horas, valor)
+    preenche(bruto, ir, inss, sindicato, liquido)
+    #data = "<h1>INSS: R$ " + str(info['inss']) + "</h1>"
 
-    liquido = bruto - ir - inss - sindicato
-
-    info = {
-        "salario_bruto": float(format(bruto, '.2f')),
-        "ir": float(format(ir, '.2f')),
-        "inss": float(format(inss, '.2f')),
-        "sindicato": float(format(sindicato, '.2f')),
-        "salario_liquido": float(format(liquido, '.2f'))
-    }
-
-    data = "<h1>INSS: R$ " + str(info['inss']) + "</h1>"
-
-    return data
+    return json.dumps({"inss": info["inss"]})
 
 @app.route('/sindicato', methods=['GET'])
 def sindicato():
@@ -130,24 +98,11 @@ def sindicato():
     else:
         return "Error: No id field provided. Please specify an id."
 
-    bruto = valor * horas
-    ir = bruto * 0.11
-    inss = bruto * 0.08
-    sindicato = bruto * 0.05
+    bruto, ir, inss, sindicato, liquido = calcula(horas, valor)
+    preenche(bruto, ir, inss, sindicato, liquido)
+    #data = "<h1>Sindicato: R$ " + str(info['sindicato']) + "</h1>"
 
-    liquido = bruto - ir - inss - sindicato
-
-    info = {
-        "salario_bruto": float(format(bruto, '.2f')),
-        "ir": float(format(ir, '.2f')),
-        "inss": float(format(inss, '.2f')),
-        "sindicato": float(format(sindicato, '.2f')),
-        "salario_liquido": float(format(liquido, '.2f'))
-    }
-
-    data = "<h1>Sindicato: R$ " + str(info['sindicato']) + "</h1>"
-
-    return data
+    return json.dumps({"sindicato": info["sindicato"]})
 
 @app.route('/liquido', methods=['GET'])
 def liquido():
@@ -158,23 +113,10 @@ def liquido():
     else:
         return "Error: No id field provided. Please specify an id."
 
-    bruto = valor * horas
-    ir = bruto * 0.11
-    inss = bruto * 0.08
-    sindicato = bruto * 0.05
+    bruto, ir, inss, sindicato, liquido = calcula(horas, valor)
+    preenche(bruto, ir, inss, sindicato, liquido)
+    #data = "<h1>Salário Líquido: R$ " + str(info['salario_liquido']) + "</h1>"
 
-    liquido = bruto - ir - inss - sindicato
-
-    info = {
-        "salario_bruto": float(format(bruto, '.2f')),
-        "ir": float(format(ir, '.2f')),
-        "inss": float(format(inss, '.2f')),
-        "sindicato": float(format(sindicato, '.2f')),
-        "salario_liquido": float(format(liquido, '.2f'))
-    }
-
-    data = "<h1>Salário Líquido: R$ " + str(info['salario_liquido']) + "</h1>"
-
-    return data
+    return json.dumps({"salario_liquido": info["salario_liquido"]})
 
 app.run(host="0.0.0.0", port="3000", debug=True)
